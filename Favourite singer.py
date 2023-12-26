bfa8b9676a98a068222646ecdabbc9f4b6744806
hari krishna <86965406+Githubhari@users.noreply.github.com>
2023-12-26 20:19:43 +0530

    Update Favourite singer.py

--- Favourite singer.py
+++ Favourite singer.py
@@ -1,1 +1,11 @@
-
+di = {}
+n = int(input())
+li = [int(x) for x in input().split()]
+for i in li:
+    di[i] = di.get(i,0) + 1
+ma = max(di.values())
+count = 0
+for i in di.values():
+    if ma == i:
+        count += 1
+print(count)
