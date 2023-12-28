f68747eea01ba2e158e62cafcaa50ccd5e555a4d
hari krishna <86965406+Githubhari@users.noreply.github.com>
2023-12-28 20:07:45 +0530

    Create Find pairs.py

---
+++ Find pairs.py
@@ -0,0 +1,14 @@
+N = int(input())
+li = [int(x) for x in input().split()]
+count = 0
+di = {}
+for i in range(N):
+    count += di.get(li[i]-i,0)
+    di[li[i]-i] = di.get(li[i]-i,0) + 1
+print(count*2)
+
+"""
+a[i]-a[j]/i-j = 1
+a[i]-a[j] = i-j
+a[i]-i = a[j]-j
+"""
